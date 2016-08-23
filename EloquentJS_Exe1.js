2a8c4e67df42ab0999cf2e3935fd15a9f8bb76da
Gabriel Sixel <gabriel.sixel@gmail.com>
2016-08-23 09:52:04 -0300

    adding the exercises statements

--- EloquentJS_Exe1.js
+++ EloquentJS_Exe1.js
@@ -1,3 +1,18 @@
+/*
+  Write a loop that makes seven calls to console.log to output the following triangle:
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+  It may be useful to know that you can find the length of a string by writing .length after it.
+  var abc = "abc";
+  console.log(abc.length);
+  // → 3
+  Most exercises contain a piece of code that you can modify to solve the exercise. Remember that you can click code blocks to edit them.
+*/
 var rigthAngleTriange = function(num) {
   var txt = "#";
   //To be able to create the exact number of lines that are requested by the function
--- EloquentJS_Exe3.js
+++ EloquentJS_Exe3.js
@@ -1,3 +1,18 @@
+/*
+  Write a program that creates a string that represents an 8×8 grid, using
+  newline characters to separate lines. At each position of the grid there is
+  either a space or a “#” character. The characters should form a chess board.
+
+  Passing this string to console.log should show something like this:
+   # # # #
+  # # # #
+   # # # #
+  # # # #
+   # # # #
+  # # # #
+   # # # #
+  # # # #
+*/
 var chessBoard = function(width, height) {
   var boardWidth = "";
   for (var j = 0; j < height; j++) {
